b6efa8489f36f66628618d267f3856fd29f054e1
xiaoandx <zw2324@vip.qq.com>
2019-10-18 15:41:11 +0800

    配置404页面

---
+++ src/main/java/club/xiaoandx/config/ErrorPageConfig.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2012-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package club.xiaoandx.config;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.boot.web.server.ErrorPageRegistrar;
+import org.springframework.boot.web.server.ErrorPageRegistry;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+
+/**
+ * @描叙：  错误页面配置
+ */
+@Configuration
+public class ErrorPageConfig implements ErrorPageRegistrar {
+
+    @Override
+    public void registerErrorPages(ErrorPageRegistry registry) {
+        /*1、按错误的类型显示错误的网页*/
+        /*错误类型为404，找不到网页的，默认显示404.html网页*/
+        ErrorPage e404 = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+        /*错误类型为500，表示服务器响应错误，默认显示500.html网页*/
+        ErrorPage e500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/index.html");
+        ErrorPage e400 = new ErrorPage(HttpStatus.BAD_REQUEST, "/index.html");
+        registry.addErrorPages(e400 ,e404, e500);
+    }
+}
