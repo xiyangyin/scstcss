3ba06782cc3a496a12ae0a98f2c14b48a9f2c8ac
xiaoandx <zw2324@vip.qq.com>
2019-10-18 20:03:06 +0800

    修改部分BUG

--- src/main/resources/static/admin/assets/js/ajxa/indexA.js
+++ src/main/resources/static/admin/assets/js/ajxa/indexA.js
@@ -4,10 +4,11 @@ $(function() {
 	$.get("/v1/open/studio/getStudioList",function(data){
 		//console.log(data);
 		for(var index in data){
-				$("#tbodyS").append("<tr class='even gradeC'><td>"+data[index].sid+"</td><td>"+data[index].sname+"</td><td style= 'overflow: hidden;white-space: nowrap;text-overflow: ellipsis;'>"+data[index].slogoPath+"</td><td class='center'>"+data[index].saddress+"</td>" +
+				$("#tbodyS").append("<tr class='even gradeC'><td>"+data[index].sid+"</td><td>"+data[index].sname+"</td><td class='center'>"+data[index].snumber+"</td><td style= 'overflow: hidden;white-space: nowrap;text-overflow: ellipsis;'>"+data[index].slogoPath+"</td><td class='center'>"+data[index].saddress+"</td>" +
 						"<td class='center'>"+data[index].sqq+"</td><td class='center' style= 'overflow: hidden;white-space: nowrap;text-overflow: ellipsis;'>"+data[index].ssummary+"</td><td class='center'><button class='btn btn-primary btn-sm' data-toggle='modal' data-target='#myModal' onclick='update("+data[index].sid+")'><i class='fa fa-edit '></i> Edit</button>&nbsp;<button class='btn btn-danger btn-sm' data-toggle='modal' data-target='#myModals' onclick='deleteStudio("+data[index].sid+")'><i class='fa fa-pencil'></i> Delete</button></td></tr>");
 		}
 	});
+	console.log(data);
 }
 );
 
