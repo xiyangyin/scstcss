e51a9bbd72bcd9fe06bcb89216e8345d4ae1644a
xiaoandx <zw2324@vip.qq.com>
2019-11-29 20:46:28 +0800

    add ssl

--- src/main/java/club/xiaoandx/SCSTCSSWebApp.java
+++ src/main/java/club/xiaoandx/SCSTCSSWebApp.java
@@ -15,10 +15,16 @@
  */
 package club.xiaoandx;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 
 /**  
  * <p>四川科技职业学院学生工作室官网(后端项目启动类)</p> 
@@ -35,5 +41,34 @@ public class SCSTCSSWebApp {
 	//通过启动内置的web容器启动项目
 	public static void main(String[] args) {
 		SpringApplication.run(SCSTCSSWebApp.class, args);
+	}
+
+	@Bean
+	public TomcatServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				SecurityConstraint constraint = new SecurityConstraint();
+				constraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				constraint.addCollection(collection);
+				context.addConstraint(constraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(httpConnector());
+		return tomcat;
+	}
+
+	@Bean
+	public Connector httpConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		//Connector监听的http的端口号
+		connector.setPort(80);
+		connector.setSecure(false);
+		//监听到http的端口号后转向到的https的端口号
+		connector.setRedirectPort(443);
+		return connector;
 	}
 }
